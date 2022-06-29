93149e322f17da044eba68c83fb5d0c609649829
CERRAA <cerraangie113@gmail.com>
2022-06-29 15:12:34 +0300

    fronted done

--- spdjangoapp/front/src/app/app.component.ts
+++ spdjangoapp/front/src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ApiService]
 })
 export class AppComponent {
-  title = 'front';
+  notes = [{title: 'test1'}];
+
+  constructor(private api: ApiService) {
+    this.getNotes(); 
+  }
+  getNotes =  () => {
+    this.api.getAllNotes().subscribe(
+      data => {
+        this.notes = data;
+      },
+      error => {
+        console.log(error);
+      }
+    )
+  }
+
+
 }
