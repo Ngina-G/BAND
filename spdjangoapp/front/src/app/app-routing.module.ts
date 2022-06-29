99dbbb5495473b8582d98cb30b458eb9000de4e5
Ngina <Nginagitau11@outlook.com>
2022-06-29 15:32:15 +0300

    feat: angular auth trial

--- spdjangoapp/front/src/app/app-routing.module.ts
+++ spdjangoapp/front/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProtectedPageComponent } from './core/authentication/protected-page/protected-page.component';
+import { LoginPageComponent } from './core/authentication/login-page/login-page.component';
+import { AuthGuardService } from './core/authentication/auth-guard.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'home', component: ProtectedPageComponent, canActivate: [AuthGuardService] },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
