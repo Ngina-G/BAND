86606899e861193d0d463fe3a5d4e8ad6edb73dd
Ngina <Nginagitau11@outlook.com>
2022-06-29 12:28:03 +0300

    feat: adds authentication

--- band/urls.py
+++ band/urls.py
@@ -4,6 +4,7 @@ from rest_framework import routers
 from api import views
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -11,5 +12,8 @@ router.register(r'users', views.UserViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path(r'api-token-auth/', obtain_jwt_token),
+    path(r'api-token-refresh/', refresh_jwt_token),
+]
++ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- spdjangoapp/front/src/app/app.module.ts
+++ spdjangoapp/front/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';    
+import { FormsModule } from '@angular/forms';    
+import { UserService } from './core/services/user.service';    
+ 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
